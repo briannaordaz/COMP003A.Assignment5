2aac32a35c4f4fbc2ad72970ca0749b26f436b10
briannaordaz <briannaordaz5@gmail.com>
2026-02-13 18:36:06 -0800

    completed steps 1 and 2

---
+++ COMP003A.Assignment5/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Introduction();
+        
+        
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        
+        
+        Console.WriteLine("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        UserInput(name, number);
+    }
+
+    static void Introduction()
+    {
+        Console.WriteLine("Welcome to C#!!");
+    }
+
+    static void UserInput(string name, int number)
+    {
+        Console.WriteLine($"Your name is {name}");
+        
+        
+        Console.WriteLine($"Your number is {number}");
+       
+        
+    }
+    
+}
