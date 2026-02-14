0a5a37b618e556a505e5b55c93afbdced5d3d66d
briannaordaz <briannaordaz5@gmail.com>
2026-02-13 19:23:46 -0800

    completed steps 5 and 6

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -8,37 +8,43 @@ class Program
     {
         Introduction();
 
-
-        Console.WriteLine("Please enter your name: ");
-        string name = Console.ReadLine();
-
-        Console.WriteLine("Please enter a number: ");
-        int number = int.Parse(Console.ReadLine());
-
-        UserInput(name, number);
-
-        
-        
-        int result = CalculationSum(number, 10);
-        Console.WriteLine($"Your result is {result}");
-        
-        
-        if (result > 15)
+        char choice;
+        do
         {
-            Console.WriteLine("Your number is greater than 15");
-        }
-        else if (result < 15)
-        {
-            Console.WriteLine("Your number is less than 15");
-        }
-        else
-        {
-            Console.WriteLine("Your number is equal to 15");
-        }
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            UserInput(name, number);
+
+
+
+            int result = CalculationSum(number, 10);
+            Console.WriteLine($"Your result is {result}");
+
+
+            if (result > 15)
+            {
+                Console.WriteLine("Your number is greater than 15");
+            }
+            else if (result < 15)
+            {
+                Console.WriteLine("Your number is less than 15");
+            }
+            else
+            {
+                Console.WriteLine("Your number is equal to 15");
+            }
             
-        
-        
-            
+            Console.WriteLine("Would you like to run the program again? (Y/N)"); 
+            choice = char.Parse(Console.ReadLine());
+
+        }
+        while (choice == 'Y' || choice == 'y');
+        Console.WriteLine("Program Ended.");
+
     }
 
     static void Introduction()
@@ -48,7 +54,7 @@ class Program
 
     static void UserInput(string name, int number)
     {
-        Console.Write($"Your name is: {name}");
+        Console.WriteLine($"Your name is: {name}");
         
         
         Console.WriteLine($"Your number is: {number}");
